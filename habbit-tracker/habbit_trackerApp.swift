b42366f64a459118b1c3839137054c5adc4993e8
Trisha Bharti <trishabharti444@gmail.com>
2026-02-23 21:45:44 +0530

    Initial Commit

---
+++ habbit-tracker/habbit_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  habbit_trackerApp.swift
+//  habbit-tracker
+//
+//  Created by Vikram Kumar on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct habbit_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
